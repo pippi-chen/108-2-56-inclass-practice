dd20c0aeff12ae4844f6a3b66c7c9689be71a4ae
pippi-chen <melody51327@gmail.com>
2020-03-18 14:46:03 +0800

    Update 0318.R

--- 410678001/0318.R
+++ 410678001/0318.R
@@ -2,7 +2,20 @@ install.packages("installr")
 require(installr)
 updateR()
 
+install.packages("rmarkdown", lib = "C:/Program Files/R/R-3.6.1/library")
+
 library(googlesheets4)
 library(dplyr)
 library(lubridate)
 
+#看以下那些有命名錯誤的存在
+my_108_total_credits <- 15
+_108_total_credits <- 15
+108_total_credits <- 15
+_my_108_total_credits <- 15
+my.108.total_credits <- 15
+.108.total_credits <- 15 # 隱藏變數
+.my.108.total_credits <- 15
+我的108學年總學分數 <- 15
+`我的108學年總學分數` <- 15 # 特殊名稱處理，`不是名稱的一部份
+`.108.total_credits` <- 15
