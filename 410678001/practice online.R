503238642ab9f49305b71f648bd7708b480f269b
pippi-chen <melody51327@gmail.com>
2020-04-02 18:22:39 +0800

    0318

--- 410678001/practice online.R
+++ 410678001/practice online.R
@@ -18,3 +18,11 @@ my_age<-我的年紀  #在某些電腦可能wrong
 library(readr)
 A02_company_all_public <- read_csv("https://www.dropbox.com/s/rtz2a9na62n3a8l/A02_company_all_public.csv?dl=1")
 A02_company_all_public$`候選人` -> candidate
+
+
+#3
+"是在" <- `哈樓` #wrong
+"是在" -> `哈樓`
+C(1:5) -> numbers #wrong
+c(1:5) -> numbers
+hello <- "world"
